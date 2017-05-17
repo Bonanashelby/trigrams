ad6fcdadca9fcba12185e2168edf1ef59a248550
Morgan Nomura <morganelle@gmail.com>
2017-05-17 15:40:18 -0700

    wrote first function, read_text, and added if name == main function

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,17 +1,25 @@
+import io
+
+
 # function that implements trigram algorithm
-def main(path, word_count):
+def main(some_file, word_count):
+    print(read_text(some_file))
+    print(200)
 
 
 # Read text and make list
-def read_text(some_string):
-    # read text
-    # remove /n, figure out something with punctation
-    # convert to list
+def read_text(some_file):
+    '''This function gets text from a file.'''
+    open_file = io.open(some_file, encoding='utf-8')
+    text = open_file.read().replace('\n', ' ')
+    open_file.close()
+    return text.split(' ')
 
 
 # Reads list and makes dictionary
 def make_dict(some_list):
     # generate trigram key/value pairs
+    pass
 
 
 # Builds story text
@@ -19,3 +27,8 @@ def make_story(word_count):
     # pick a starting place
     # find trigrams in dictionary
     # if no trigram and story DNE word_count, pick
+    pass
+
+
+if __name__ == '__main__':
+    main('../poe_test.txt', 200)
