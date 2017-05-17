9fe942c77b6095bed7d186dcac543bc4549c6c2a
Morgan Nomura <morganelle@gmail.com>
2017-05-17 14:59:01 -0700

    pseudo-code and function scaffolding for trigrams.py

---
+++ src/trigrams.py
@@ -0,0 +1,21 @@
+# function that implements trigram algorithm
+def main(path, word_count):
+
+
+# Read text and make list
+def read_text(some_string):
+    # read text
+    # remove /n, figure out something with punctation
+    # convert to list
+
+
+# Reads list and makes dictionary
+def make_dict(some_list):
+    # generate trigram key/value pairs
+
+
+# Builds story text
+def make_story(word_count):
+    # pick a starting place
+    # find trigrams in dictionary
+    # if no trigram and story DNE word_count, pick
