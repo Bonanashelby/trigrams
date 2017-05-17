f38ad8e520471caa444d582689ae41b5944e9d5e
bonanashelby <bonanashelby@gmail.com>
2017-05-17 15:43:39 -0700

    wrote in comments for test scaffolding

---
+++ src/test_trigrams.py
@@ -0,0 +1,8 @@
+"""This is our tests for our Trigrams."""
+
+
+#Test input for word_count = word_count  - that 200 words is 200 words
+def main(path, word_count):
+
+
+#Test for /n , spaces  then punctuation
